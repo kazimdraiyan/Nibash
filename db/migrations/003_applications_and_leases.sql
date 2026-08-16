6e01a8868300b997d1a1c268fa6558092d6f4a15
Raiyan <kazimdraiyan2006@gmail.com>
2026-08-16 20:44:49 +0600

    style: format sql files

--- db/migrations/003_applications_and_leases.sql
+++ db/migrations/003_applications_and_leases.sql
@@ -1,4 +1,5 @@
 drop table if exists contracts cascade;
+
 drop table if exists applies cascade;
 
 create table applies(
@@ -8,13 +9,13 @@ create table applies(
     applied_at timestamp not null default current_timestamp,
     status varchar(20) not null default 'pending' check (status in ('approved', 'pending', 'rejected'))
 );
+
 create table contracts(
     id serial primary key,
     tenant_id int not null references tenants(user_id) on delete cascade,
     listing_id int not null references listings(id) on delete cascade,
     agreement_id int unique references agreements(terms_id) on delete cascade,
-    
-    status varchar(20) not null default 'proposed' check (status in ('proposed', 'signed','completed')),
+    status varchar(20) not null default 'proposed' check (status in ('proposed', 'signed', 'completed')),
     start_date date not null,
     end_date date not null,
     paid_security_deposit boolean not null default false
